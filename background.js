e7520dc04fd426b74fb5a27a542b797471443ddc
Nicholas N. <nicholasng99@hotmail.com>
2026-07-20 02:29:00 +0800

    v1.1: remove 3s startup delay

--- background.js
+++ background.js
@@ -1,5 +1,4 @@
 browser.runtime.onStartup.addListener(async () => {
-  await new Promise(r => setTimeout(r, 3000));
   const [active] = await browser.tabs.query({ active: true, currentWindow: true });
   const pinned = await browser.tabs.query({ pinned: true });
   for (const tab of pinned) {
